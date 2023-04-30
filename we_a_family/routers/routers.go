6b09c35dfe3e7d85ccfa89b74b703bb5423c26d2
gh <1017835245@qq.com>
2023-04-30 22:10:56 +0800

    update project structure

--- we_a_family/routers/routers.go
+++ we_a_family/routers/routers.go
@@ -3,32 +3,46 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 	"we_a_family/we_a_family/models"
 )
 
 func InitRouters() *gin.Engine {
 	Router := gin.Default()
-	Router.POST("/insertone", func(c *gin.Context) {
-		Models.InsertOneMember("lizhen", "lz")
-		m, _ := Models.FindOneMember("lizhen")
+	Router.POST("/insertone/:username/:password", func(c *gin.Context) {
+		username := c.Param("username")
+		password := c.Param("password")
+		Models.InsertOneMember(username, password)
+		m, _ := Models.FindOneMember(username)
 		c.JSON(http.StatusOK, m.Id)
 	})
-	Router.POST("/findone", func(c *gin.Context) {
-		m, _ := Models.FindOneMember("genghao")
+	Router.GET("/findone/:username", func(c *gin.Context) {
+		username := c.Param("username")
+		m, _ := Models.FindOneMember(username)
 		c.JSON(http.StatusOK, m.Id)
 	})
-	Router.POST("/findall", func(c *gin.Context) {
+	Router.GET("/findall", func(c *gin.Context) {
 		m := Models.FindsAllMember()
-		c.JSON(http.StatusOK, m[0].Id)
+		c.JSON(http.StatusOK, m[1].Id)
 	})
-	Router.POST("/deleteone", func(c *gin.Context) {
-		Models.DelOneMember(1)
-		m, _ := Models.FindOneMember("genghao")
+	Router.PATCH("/deleteone/:id/:username", func(c *gin.Context) {
+		id := c.Param("id")
+		username := c.Param("username")
+		id1, _ := strconv.Atoi(id)
+		Models.DelOneMember(id1)
+		m, _ := Models.FindOneMember(username)
 		c.JSON(http.StatusOK, m.Deleted)
 	})
-	Router.POST("/updatedone", func(c *gin.Context) {
-		Models.UpdateOneMember(1, "genghao", "genghao", false)
-		m, _ := Models.FindOneMember("genghao")
+	Router.PATCH("/updatedone/:id/:username/:password/:delete", func(c *gin.Context) {
+		id := c.Param("id")
+		id1, _ := strconv.Atoi(id)
+		username := c.Param("username")
+		password := c.Param("password")
+		delete := c.Param("delete")
+		delete1, _ := strconv.ParseBool(delete)
+
+		Models.UpdateOneMember(id1, username, password, delete1)
+		m, _ := Models.FindOneMember(username)
 		c.JSON(http.StatusOK, m.Password)
 	})
 	return Router
